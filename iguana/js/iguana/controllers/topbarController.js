44e457fb913e88f92b30c482f34b8811b43f6490
Fariss Abdo <abdelhakfarissi@gmail.com>
2016-02-24 18:46:47 +0000

    add logout function

--- iguana/js/iguana/controllers/topbarController.js
+++ iguana/js/iguana/controllers/topbarController.js
@@ -1,6 +1,23 @@
 'use strict';
 
-angular.module('iguanaApp.controllers').controller('topbarController', function(go, $rootScope) { 
+angular.module('iguanaApp.controllers').controller('topbarController', 
+  function(go, $state, $rootScope, naclAPI) { 
+
+  console.info('topbarController - activeHandle: ' + $rootScope.activeHandle);
+
+  if (! $rootScope.activeHandle) {
+    go.logInPage();
+  }
+
+  this.logout = function() {
+    console.info("logout...");
+    naclAPI.makeRequest($rootScope.nacl_request.logout, function(request, response) {
+      console.info("SuperNET logout Response:");
+      console.info(response);
+      $rootScope.activeHandle = false;
+      go.logInPage();
+    });
+  };
 
   this.goHome = function() {
     go.walletHome();
@@ -14,8 +31,8 @@ angular.module('iguanaApp.controllers').controller('topbarController', function(
     if(peer)
       $rootScope.showPeerFavorites = false;
   };
+  
   this.changeFavorite = function(){
-    
     $rootScope.peerFavorites = this.peerFavorites;
   };
   
