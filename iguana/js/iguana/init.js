c55118b25ed52248fcb1486ed7b3718cd412d5d5
Fariss Abdo <abdelhakfarissi@gmail.com>
2016-02-26 19:22:08 +0000

    add Profile Object that stores the user credentials

--- iguana/js/iguana/init.js
+++ iguana/js/iguana/init.js
@@ -965,6 +965,48 @@ root.makeRequest = function( request, callback ) {
     return root;
   });
 
+'use strict';
+
+/**
+ * Profile
+ *
+ * credential: array of OBJECTS
+ */
+function Profile() {
+  this.version = '1.0.0';
+};
+
+Profile.create = function(opts) {
+  opts = opts || {};
+
+  var x = new Profile();
+  x.createdOn = Date.now();
+  x.credentials = opts.credentials || [];
+  x.disclaimerAccepted = false;
+  return x;
+};
+
+Profile.fromObj = function(obj) {
+  var x = new Profile();
+
+  x.createdOn = obj.createdOn;
+  x.credentials = obj.credentials;
+  x.disclaimerAccepted = obj.disclaimerAccepted;
+
+  if (x.credentials[0] && typeof x.credentials[0] != 'object')
+    throw ("credentials should be an object");
+
+  return x;
+};
+
+Profile.fromString = function(str) {
+  return Profile.fromObj(JSON.parse(str));
+};
+
+Profile.prototype.toObj = function() {
+  return JSON.stringify(this);
+};
+
 
 
 'use strict';
