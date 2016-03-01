26ff7e40dff9cfaf5f6181b354ca7e164043393e
Tural Mehdi <alitural.mehdi@gmail.com>
2016-03-01 01:04:50 +0000

    new account page markup

--- iguana/js/iguana/controllers/signupController.js
+++ iguana/js/iguana/controllers/signupController.js
@@ -2,11 +2,18 @@
 
 angular.module('iguanaApp.controllers').controller('signupController',
   function($rootScope, $scope, $state, $http, $timeout, $log, naclAPI, pphgen, go, storageService){
-
+  
    $scope.username = "";
    $scope.password = "";
    $scope.passphrase = pphgen.GeneratePassPhrase();
-   
+   $scope.passphraseConfirm = "";
+   $scope.firstStep;
+   $scope.generatePassword = function(){
+       $scope.passphrase = pphgen.GeneratePassPhrase();
+   };
+   $scope.backToLogin = function(){
+       $rootScope.go('logIn');
+   };
    $scope.signup = function() {};
 
   });
